183a78e02c2450789b1c2be2479cb6ca94f18827
Apoorvi Dwivedi <apurvid@gmail.com>
2023-08-04 01:18:08 +0100

    add supporting files and updating main

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # main.py
+import torchaudio
 
 from preprocessing.audiofeature import MelSpectrogramTransform
 from preprocessing.audiofeature import AudioEmotionDataset
 from preprocessing.audiofeature import split_dataset_folds
 from torch.utils.data import DataLoader
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from training.model import MTSA
 
 # Define data folder and CSV file path
 data_folder = 'Data'
@@ -37,3 +42,94 @@ for i, fold in enumerate(data_loaders, 1):
     print(f"Train data: {len(fold['train'].dataset)} samples")
     print(f"Test data: {len(fold['test'].dataset)} samples")
     print("-" * 30)
+
+
+# Initialize your custom model
+custom_model = MTSA()
+
+# Load the state_dict of the pre-trained model
+pretrained_model_path = 'C:/Users/apurv/Desktop/project/pre-trained model/ponswon.pth'
+
+pretrained_model_state = torch.load(pretrained_model_path, map_location=torch.device('cpu'))
+
+# Get the state_dict of the custom model
+custom_model_dict = custom_model.state_dict()
+
+# Filter out the layers that exist in both models
+pretrained_dict = {k: v for k, v in pretrained_model_state.items() if k in custom_model_dict}
+
+# Update the state_dict of the custom model
+custom_model_dict.update(pretrained_dict)
+
+# Load the updated state_dict into the custom model
+custom_model.load_state_dict(custom_model_dict)
+
+
+# Modify the last layer for your task
+num_classes = 4
+custom_model.classifier = nn.Linear(custom_model.classifier.in_features, num_classes)
+
+# Freeze all layers except the last one
+for param in custom_model.parameters():
+    param.requires_grad = False
+
+# Allow the last layer to be trainable
+custom_model.classifier.requires_grad = True
+
+# Move the model to the appropriate device (GPU/CPU)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+custom_model.to(device)
+
+# Define the loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(custom_model.parameters(), lr=0.001)  # Adjust learning rate as needed
+
+# Set the number of epochs
+num_epochs = 10  # Adjust the number of epochs as needed
+
+torchaudio.set_audio_backend("soundfile")
+
+file_path = 'C:/Users/apurv/Desktop/project/Data/piano/sad/284_sad_n1_i1_piano_AlbLin_20230126.wav'
+try:
+    waveform, sample_rate = torchaudio.load(file_path)
+    print(f"Loaded audio file: {file_path}")
+except Exception as e:
+    print(f"Error loading file {file_path}: {e}")
+
+
+
+
+# Training loop
+for epoch in range(num_epochs):
+    for phase in ['train', 'test']:
+        if phase == 'train':
+            custom_model.train()
+        else:
+            custom_model.eval()
+
+        running_loss = 0.0
+        corrects = 0
+
+        data_loader = data_loaders[0][phase] if phase == 'train' else data_loaders[1][phase]
+
+        for inputs, labels in data_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            with torch.set_grad_enabled(phase == 'train'):
+                outputs = custom_model(inputs)
+                loss = criterion(outputs, labels)
+
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+            running_loss += loss.item() * inputs.size(0)
+            _, preds = torch.max(outputs, 1)
+            corrects += torch.sum(preds == labels.data)
+
+        epoch_loss = running_loss / len(data_loader.dataset)
+        epoch_accuracy = corrects.double() / len(data_loader.dataset)
+
+        print(f'{phase} - Loss: {epoch_loss:.4f} Acc: {epoch_accuracy:.4f}')
--- preprocessing/audiofeature.py
+++ preprocessing/audiofeature.py
@@ -6,6 +6,9 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import StratifiedKFold
 
+torchaudio.set_audio_backend("soundfile")
+
+
 class MelSpectrogramTransform:
     def __init__(self, sample_rate=16000, n_fft=400, win_length=400, hop_length=160, n_mels=128):
         self.sample_rate = sample_rate
@@ -35,13 +38,14 @@ class MelSpectrogramTransform:
 
         return mel_spec_normalized
 
+
 # Usage example:
 # Create an instance of MelSpectrogramTransform
+
 mel_transform = MelSpectrogramTransform()
-
-
 # Define emotion labels (modify as needed)
 EMOTIONS = ['aggressive', 'relaxed', 'happy', 'sad']
+
 
 class AudioEmotionDataset(Dataset):
     def __init__(self, data_folder, csv_file, transform=None):
@@ -57,7 +61,13 @@ class AudioEmotionDataset(Dataset):
     def __getitem__(self, idx):
         # Load audio data and emotion label for the given index
         file_path = os.path.join(self.data_folder, self.data[idx])
-        waveform, sample_rate = torchaudio.load(file_path)
+        print(file_path)
+        try:
+            waveform, sample_rate = torchaudio.load(file_path)
+        except Exception as e:
+            # Handle the error here, you can print an error message or raise an exception.
+            print(f"Error loading file {file_path}: {e}")
+            return None
 
         # Apply audio transformations (e.g., compute mel spectrogram)
         if self.transform:
@@ -71,17 +81,17 @@ class AudioEmotionDataset(Dataset):
         return mel_spectrogram, emotion_label
 
     def _load_data_from_csv(self, csv_file):
-      data = []
-      labels = []
-      df = pd.read_csv(csv_file)
-      #print(df)
-      for _, row in df.iterrows():
-          # Assuming the file_name is in the 'file_name' column and the emotion is in the 'emotion' column
-          file_name = row['file_name']
-          emotion = row['emotion']
-          data.append(file_name)
-          labels.append(emotion)
-      return data, labels
+        data = []
+        labels = []
+        df = pd.read_csv(csv_file)
+        print(df)
+        for _, row in df.iterrows():
+            # Assuming the file_name is in the 'file_name' column and the emotion is in the 'emotion' column
+            file_name = row['file_name']
+            emotion = row['emotion']
+            data.append(file_name)
+            labels.append(emotion)
+        return data, labels
 
 
 def split_dataset_folds(dataset, num_folds=5, random_state=None):
@@ -99,9 +109,11 @@ def split_dataset_folds(dataset, num_folds=5, random_state=None):
 
     return fold_splits
 
+
 # Usage example:
 # Assuming you have already created the AudioEmotionDataset instance 'audio_dataset'
 # and defined the MelSpectrogramTransform 'mel_transform'
-audio_dataset = AudioEmotionDataset('C:/Users/apurv/Desktop/project/Data/piano', 'C:/Users/apurv/Desktop/project/Data/piano/annotations_piano.csv', transform=mel_transform)
+audio_dataset = AudioEmotionDataset('C:/Users/apurv/Desktop/project/Data/piano',
+                                    'C:/Users/apurv/Desktop/project/Data/piano/annotations_piano.csv',
+                                    transform=mel_transform)
 fold_splits = split_dataset_folds(audio_dataset)
-
